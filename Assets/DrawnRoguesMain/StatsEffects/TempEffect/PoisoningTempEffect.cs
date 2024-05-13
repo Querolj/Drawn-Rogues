733fe5b229edd58e95faa4f02c31bd2ba0f18296
Jean Querol <queroljean22@gmail.com>
2024-05-13 11:31:13 +0200

    fix contour sorting order & ajust heal and poisonning effect

--- Assets/DrawnRoguesMain/StatsEffects/TempEffect/PoisoningTempEffect.cs
+++ Assets/DrawnRoguesMain/StatsEffects/TempEffect/PoisoningTempEffect.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 [CreateAssetMenu (fileName = "PoisoningTempEffect", menuName = "TempEffect/PoisoningTempEffect", order = 1)]
 public class PoisoningTempEffect : TempEffect
 {
+    [SerializeField]
+    private float _poisonDamagePercentage = 0.07f;
     public override void Apply (Transform ownerTransform, string ownerName, AttackableStats ownerStats, FightRegistry fightDescription, Action onAnimeEnded)
     {
         PlayAnimation (ownerTransform.position,
             () =>
             {
-                int poisonDamage = (int) (ownerStats.Life * 0.1f);
+                int poisonDamage = (int) (ownerStats.Life * _poisonDamagePercentage);
                 poisonDamage = Math.Max (poisonDamage, 1);
                 fightDescription.Report (fightDescription.GetColoredAttackableName (ownerName, ownerTransform.tag) + " took <b>" + poisonDamage + "</b> damage from poisoning.");
                 ownerStats.AttackableState.ReceiveDamage (poisonDamage);
